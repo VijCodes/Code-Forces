2dc08b967334716d9f34a9e0134c08f6b3241bc6
VijCodes <kattavy@mail.uc.edu>
2023-10-09 15:56:05 -0500

    Adding code forces code

---
+++ Directed Edge Labelled Graph.py
@@ -0,0 +1,28 @@
+b4-
+from collections import defaultdict
+class Node():
+    def __init__(self,val = None):
+        self.connections = defaultdict(lambda: defaultdict(Node))
+        self.val = val
+class DELG():
+    def __init__(self):
+        self.nodes = defaultdict(Node)
+    def addNode(self,value):
+        self.nodes[value] = Node(value)
+    def addConnection(self,start,end,label):
+        if start not in self.nodes:
+            print(f'Node:{start} is not found in the graph. Unable to add connection {start}-->-{label}-->-{end}')
+            return 
+        if end not in self.nodes:
+            self.addNode(end)
+        self.nodes[start].connections[label][end] = self.nodes[end]
+       
+
+NewGraph = DELG()
+NewGraph.addNode('Hyderabad')
+NewGraph.addConnection('Hyderabad','AP','capital')
+
+
+
+    
+
